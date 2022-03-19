c8fd7d31061571a71ab17e663b421dcfbf44bf88
RhAnjiE <irhanjie@gmail.com>
2022-03-19 14:56:37 +0100

    Add Lab 1

--- src/main/kotlin/Lab1/Zad1.kt
+++ src/main/kotlin/Lab1/Zad1.kt
@@ -1,5 +1,24 @@
 package Lab1
 
+fun hanoiTower(number: Int, source: Char, destination: Char, auxiliary: Char) {
+    if (number == 1) {
+        println("Przenoszę dysk [1]: [$source] -> [$destination]")
+        return
+    }
+
+    hanoiTower(number - 1, source, auxiliary, destination)
+
+    println("Przenoszę dysk [$number]: [$source] -> [$destination]")
+
+    hanoiTower(number - 1, auxiliary, destination, source)
+}
+
 fun main(args: Array<String>) {
-    println("Lab 1!")
+    println("Marcin Dyla - Lab1 - Wieza Hanoi\n")
+
+    print("Podaj ilosc dyskow na wiezy startowej: ")
+    val text = readLine()
+    val number: Int = text!!.toInt()
+
+    hanoiTower(number, 'A', 'C', 'B')
 }
